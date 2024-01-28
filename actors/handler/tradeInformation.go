00d4cb3d29bdab890a7b9b2cd2cff5372a57f44f
tkhrk <>
2024-01-28 17:56:04 +0900

    mod: refactor

--- actors/handler/tradeInformation.go
+++ actors/handler/tradeInformation.go
@@ -15,93 +15,68 @@ type TradeInformationHandler struct {
 	system *actor.ActorSystem
 }
 
-
-type CombinedItemDetails struct {
-	AveragePurchasingPrice float32 `json:"averagePurchasingPrice"`
-	Count                  int     `json:"count"`
-	MarketPrice            float32 `json:"marketPrice"`
-}
-
-type CombinedResponse struct {
-	Items map[string]CombinedItemDetails `json:"items"`
-}
-
 func NewTradeInformationHandler() *TradeInformationHandler {
 	return &TradeInformationHandler{
 		system: actor.NewActorSystem(),
 	}
 }
 
-func (handler *TradeInformationHandler) GetTradeInformation() ([]byte, error) {
-	orangeInventoryProps := actor.PropsFromProducer(func() actor.Actor { return &collector.OrangeActor{} })
-	orangeInventoryCollector := handler.system.Root.Spawn(orangeInventoryProps)
+func (handler *TradeInformationHandler) spawnActor(producer func() actor.Actor) *actor.PID {
+	props := actor.PropsFromProducer(producer)
+	return handler.system.Root.Spawn(props)
+}
 
-	AppleInventoryProps := actor.PropsFromProducer(func() actor.Actor { return &collector.AppleActor{} })
-	AppleInventoryCollector := handler.system.Root.Spawn(AppleInventoryProps)
+func (handler *TradeInformationHandler) requestActorResponse(props *actor.Props, request interface{}, timeout time.Duration) (interface{}, error) {
+	actorRef := handler.system.Root.Spawn(props)
+	future := handler.system.Root.RequestFuture(actorRef, request, timeout)
+	return future.Result()
+}
 
-	bananaInventoryProps := actor.PropsFromProducer(func() actor.Actor { return &collector.BananaActor{} })
-	bananaInventoryCollector := handler.system.Root.Spawn(bananaInventoryProps)
+func (handler *TradeInformationHandler) collectInventoryInfo() (*aggregator.AggregateResponse, error) {
+	orangeInventoryCollector := handler.spawnActor(func() actor.Actor { return &collector.OrangeActor{} })
+	appleInventoryCollector := handler.spawnActor(func() actor.Actor { return &collector.AppleActor{} })
+	bananaInventoryCollector := handler.spawnActor(func() actor.Actor { return &collector.BananaActor{} })
 
 	aggregatorProps := actor.PropsFromProducer(func() actor.Actor {
 		return &aggregator.AggregatorActor{
 			Collectors: map[string]*actor.PID{
-				"appleInventory":  AppleInventoryCollector,
+				"appleInventory":  appleInventoryCollector,
 				"orangeInventory": orangeInventoryCollector,
 				"bananaInventory": bananaInventoryCollector,
 			},
 		}
 	})
-	aggrgtr := handler.system.Root.Spawn(aggregatorProps)
 
-	future := handler.system.Root.RequestFuture(aggrgtr, &aggregator.AggregateRequest{
+	result, err := handler.requestActorResponse(aggregatorProps, &aggregator.AggregateRequest{
 		ItemNames: []string{"orangeInventory", "appleInventory", "bananaInventory"},
 	}, 10*time.Second)
-
-	result, err := future.Result()
 	if err != nil {
-		fmt.Printf("Error while waiting for result: %s\n", err)
 		return nil, err
 	}
 
-	aggregatorResponse, ok := result.(*aggregator.AggregateResponse)
+	aggResp, ok := result.(*aggregator.AggregateResponse)
 	if !ok {
-		fmt.Println("Invalid response type")
-		return nil, err
+		return nil, fmt.Errorf("invalid response type")
 	}
-
-	//
-	// market
-	//
-	marketProps := actor.PropsFromProducer(func() actor.Actor { return &market.Actor{} })
-	marketActor := handler.system.Root.Spawn(marketProps)
-	marketFuture := handler.system.Root.RequestFuture(marketActor, &market.Request{
-		ItemNames: []string{"apple", "orange", "banana"},
-	}, 10*time.Second)
-
-	marketResult, err := marketFuture.Result()
-	if err != nil {
-		fmt.Printf("Error while waiting for market result: %s\n", err)
-		return nil, err
-	}
-
-	marketResponse, ok := marketResult.(*market.Response)
-	if !ok {
-		fmt.Println("Invalid market response type")
-		return nil, err
-	}
-
-	combinedResponse := CombineResponses(aggregatorResponse, marketResponse)
-
-	jsonResponse, err := json.Marshal(combinedResponse)
-	if err != nil {
-		fmt.Println("Error marshaling JSON:", err)
-		return nil, err
-	}
-	
-	return jsonResponse, nil
-	
+	return aggResp, nil
 }
 
+func (handler *TradeInformationHandler) collectMarketInfo() (*market.Response, error) {
+	marketProps := actor.PropsFromProducer(func() actor.Actor { return &market.Actor{} })
+
+	result, err := handler.requestActorResponse(marketProps, &market.Request{
+		ItemNames: []string{"apple", "orange", "banana"},
+	}, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	mktResp, ok := result.(*market.Response)
+	if !ok {
+		return nil, fmt.Errorf("invalid response type")
+	}
+	return mktResp, nil
+}
 
 func CombineResponses(aggregatorResponse *aggregator.AggregateResponse, marketResponse *market.Response) CombinedResponse {
 	combined := CombinedResponse{
@@ -117,10 +92,42 @@ func CombineResponses(aggregatorResponse *aggregator.AggregateResponse, marketRe
 
 	for itemName, marketItemDetails := range marketResponse.Results {
 		if item, exists := combined.Items[itemName]; exists {
-			item.MarketPrice = marketItemDetails.MarketPrice 
+			item.MarketPrice = marketItemDetails.MarketPrice
 			combined.Items[itemName] = item
 		}
 	}
 
 	return combined
+}
+
+func (handler *TradeInformationHandler) GetTradeInformation() ([]byte, error) {
+	aggregatorResponse, err := handler.collectInventoryInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	marketResponse, err := handler.collectMarketInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	combinedResponse := CombineResponses(aggregatorResponse, marketResponse)
+
+	jsonResponse, err := json.Marshal(combinedResponse)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return nil, err
+	}
+
+	return jsonResponse, nil
+}
+
+type CombinedItemDetails struct {
+	AveragePurchasingPrice float32 `json:"averagePurchasingPrice"`
+	Count                  int     `json:"count"`
+	MarketPrice            float32 `json:"marketPrice"`
+}
+
+type CombinedResponse struct {
+	Items map[string]CombinedItemDetails `json:"items"`
 }
