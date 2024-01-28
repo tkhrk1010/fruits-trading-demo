84135f27c19dfc39d87759669c4458eb0d2b2158
tkhrk <>
2024-01-28 18:16:14 +0900

    mod: refactor

---
+++ actors/handler/market.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"time"
+	"fmt"
+
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/tkhrk1010/fruits-trading-demo/actors/market"
+)
+
+// CollectMarketInfo collects market information.
+func (handler *TradeInformationHandler) collectMarketInfo() (*market.Response, error) {
+	marketProps := actor.PropsFromProducer(func() actor.Actor { return &market.Actor{} })
+
+	result, err := handler.requestActorResponse(marketProps, &market.Request{
+		ItemNames: []string{"apple", "orange", "banana"},
+	}, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	mktResp, ok := result.(*market.Response)
+	if !ok {
+		return nil, fmt.Errorf("invalid response type")
+	}
+	return mktResp, nil
+}
--- actors/handler/tradeInformation.go
+++ actors/handler/tradeInformation.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/asynkron/protoactor-go/actor"
 	"github.com/tkhrk1010/fruits-trading-demo/actors/inventory/aggregator"
-	"github.com/tkhrk1010/fruits-trading-demo/actors/inventory/collector"
 	"github.com/tkhrk1010/fruits-trading-demo/actors/market"
 )
 
@@ -15,89 +14,14 @@ type TradeInformationHandler struct {
 	system *actor.ActorSystem
 }
 
-func NewTradeInformationHandler() *TradeInformationHandler {
-	return &TradeInformationHandler{
-		system: actor.NewActorSystem(),
-	}
+type CombinedItemDetails struct {
+	AveragePurchasingPrice float32 `json:"averagePurchasingPrice"`
+	Count                  int     `json:"count"`
+	MarketPrice            float32 `json:"marketPrice"`
 }
 
-func (handler *TradeInformationHandler) spawnActor(producer func() actor.Actor) *actor.PID {
-	props := actor.PropsFromProducer(producer)
-	return handler.system.Root.Spawn(props)
-}
-
-func (handler *TradeInformationHandler) requestActorResponse(props *actor.Props, request interface{}, timeout time.Duration) (interface{}, error) {
-	actorRef := handler.system.Root.Spawn(props)
-	future := handler.system.Root.RequestFuture(actorRef, request, timeout)
-	return future.Result()
-}
-
-func (handler *TradeInformationHandler) collectInventoryInfo() (*aggregator.AggregateResponse, error) {
-	orangeInventoryCollector := handler.spawnActor(func() actor.Actor { return &collector.OrangeActor{} })
-	appleInventoryCollector := handler.spawnActor(func() actor.Actor { return &collector.AppleActor{} })
-	bananaInventoryCollector := handler.spawnActor(func() actor.Actor { return &collector.BananaActor{} })
-
-	aggregatorProps := actor.PropsFromProducer(func() actor.Actor {
-		return &aggregator.AggregatorActor{
-			Collectors: map[string]*actor.PID{
-				"appleInventory":  appleInventoryCollector,
-				"orangeInventory": orangeInventoryCollector,
-				"bananaInventory": bananaInventoryCollector,
-			},
-		}
-	})
-
-	result, err := handler.requestActorResponse(aggregatorProps, &aggregator.AggregateRequest{
-		ItemNames: []string{"orangeInventory", "appleInventory", "bananaInventory"},
-	}, 10*time.Second)
-	if err != nil {
-		return nil, err
-	}
-
-	aggResp, ok := result.(*aggregator.AggregateResponse)
-	if !ok {
-		return nil, fmt.Errorf("invalid response type")
-	}
-	return aggResp, nil
-}
-
-func (handler *TradeInformationHandler) collectMarketInfo() (*market.Response, error) {
-	marketProps := actor.PropsFromProducer(func() actor.Actor { return &market.Actor{} })
-
-	result, err := handler.requestActorResponse(marketProps, &market.Request{
-		ItemNames: []string{"apple", "orange", "banana"},
-	}, 10*time.Second)
-	if err != nil {
-		return nil, err
-	}
-
-	mktResp, ok := result.(*market.Response)
-	if !ok {
-		return nil, fmt.Errorf("invalid response type")
-	}
-	return mktResp, nil
-}
-
-func CombineResponses(aggregatorResponse *aggregator.AggregateResponse, marketResponse *market.Response) CombinedResponse {
-	combined := CombinedResponse{
-		Items: make(map[string]CombinedItemDetails),
-	}
-
-	for itemName, aggDetails := range aggregatorResponse.Results {
-		combined.Items[itemName] = CombinedItemDetails{
-			AveragePurchasingPrice: aggDetails.AveragePurchasingPrice,
-			Count:                  aggDetails.Count,
-		}
-	}
-
-	for itemName, marketItemDetails := range marketResponse.Results {
-		if item, exists := combined.Items[itemName]; exists {
-			item.MarketPrice = marketItemDetails.MarketPrice
-			combined.Items[itemName] = item
-		}
-	}
-
-	return combined
+type CombinedResponse struct {
+	Items map[string]CombinedItemDetails `json:"items"`
 }
 
 func (handler *TradeInformationHandler) GetTradeInformation() ([]byte, error) {
@@ -122,12 +46,43 @@ func (handler *TradeInformationHandler) GetTradeInformation() ([]byte, error) {
 	return jsonResponse, nil
 }
 
-type CombinedItemDetails struct {
-	AveragePurchasingPrice float32 `json:"averagePurchasingPrice"`
-	Count                  int     `json:"count"`
-	MarketPrice            float32 `json:"marketPrice"`
+
+func NewTradeInformationHandler() *TradeInformationHandler {
+	return &TradeInformationHandler{
+		system: actor.NewActorSystem(),
+	}
 }
 
-type CombinedResponse struct {
-	Items map[string]CombinedItemDetails `json:"items"`
+func (handler *TradeInformationHandler) spawnActor(producer func() actor.Actor) *actor.PID {
+	props := actor.PropsFromProducer(producer)
+	return handler.system.Root.Spawn(props)
 }
+
+func (handler *TradeInformationHandler) requestActorResponse(props *actor.Props, request interface{}, timeout time.Duration) (interface{}, error) {
+	actorRef := handler.system.Root.Spawn(props)
+	future := handler.system.Root.RequestFuture(actorRef, request, timeout)
+	return future.Result()
+}
+
+func CombineResponses(aggregatorResponse *aggregator.AggregateResponse, marketResponse *market.Response) CombinedResponse {
+	combined := CombinedResponse{
+		Items: make(map[string]CombinedItemDetails),
+	}
+
+	for itemName, aggDetails := range aggregatorResponse.Results {
+		combined.Items[itemName] = CombinedItemDetails{
+			AveragePurchasingPrice: aggDetails.AveragePurchasingPrice,
+			Count:                  aggDetails.Count,
+		}
+	}
+
+	for itemName, marketItemDetails := range marketResponse.Results {
+		if item, exists := combined.Items[itemName]; exists {
+			item.MarketPrice = marketItemDetails.MarketPrice
+			combined.Items[itemName] = item
+		}
+	}
+
+	return combined
+}
+
